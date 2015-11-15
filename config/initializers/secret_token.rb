17af2d560dd28120395f3aeb79b356032f3ddb2a
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2015-11-15 16:44:15 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WalmartTestTask::Application.config.secret_token = '7f844e6fc9bce818e591b76862265a5308e7dd92df62e451990d0acf44648086e3303067ae422055ceaf2717d7fc4e9d4cffe05d7696ce4566f144e399c2b671'
